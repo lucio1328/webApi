e06d13cd13d5518be4bc76e5dc4608c860f17227
AlainPatrick003 <152412702+AlainPatrick003@users.noreply.github.com>
2025-01-28 14:01:48 +0300

    oko kokok ok ok ok

--- src/main/java/com/web/api/controllers/UtilisateurController.java
+++ src/main/java/com/web/api/controllers/UtilisateurController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.web.api.models.Config;
 import com.web.api.models.PreInscriptionEntity;
-import com.web.api.models.Sessions;
 import com.web.api.models.TokenUserEntity;
 import com.web.api.models.UtilisateurEntity;
 import com.web.api.repository.ConfigRepository;
